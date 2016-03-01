321b7a9f8edf5044fe8b057483ad240d415c759e
Aaron Greenspan <afg419@gmail.com>
2016-03-01 14:34:33 -0700

    Styles page to show that post has been submitted

--- app/assets/javascripts/sliders.js
+++ app/assets/javascripts/sliders.js
@@ -10,20 +10,36 @@ var extractEntry = function (){
     values["body"] = $('#journal_entry_body')[0].value
     values["user_id"] = $('.user').attr('name')
 
-    debugger
-
     $.ajax({
       type: "POST",
       url: "/journal_entries",
       data: values,
       success: function(msg){
-        renderPost(msg);
+        lockPost(msg);
       },
       error: function(error_message){
         console.log(error_message.responseText)
       }
     });
-
-
   })
+}
+
+
+var lockPost = function(msg){
+  if (msg.created == "success"){
+    $('.slider').each(function(index, slide){
+      $(slide).attr('readonly', "readonly")
+      $(slide).css('background-color', "black")
+    })
+    $('#tag').attr('readonly', "readonly")
+    $('#tag').css('color', "#85A7D0")
+    $('#journal_entry_body').attr('readonly', "readonly")
+    $('#journal_entry_body').css('color', "#85A7D0")
+    $('#header').empty()
+    $('#header').append("Your Journal Entry has been Submitted")
+    $('#new-entry-main-button').empty()
+    $('#new-entry-main-button').append("<a href='/dashboard'>To Dashboard</a>")
+  }else{
+    console.log("message was not created")
+  }
 }
--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -8,17 +8,22 @@ class JournalEntriesController < ApplicationController
   end
 
   def create
-    binding.pry
     user_id = current_user.id
-    new_entry = google_service.create_file(journal_params[:tag])
-    `touch entry#{user_id}.txt`
-    `echo "#{journal_params[:body]}" >> entry#{user_id}.txt`
-    reply = google_service.drive.insert_file(new_entry,
-                                             upload_source: "entry#{user_id}.txt",
-                                             content_type: 'text/plain')
-    `rm entry#{user_id}.txt`
-    @entry = JournalEntry.create(file_id: reply.id, user: current_user)
-    redirect_to journal_entry_path(@entry)
+    if journal_params["user_id"].to_i == current_user.id
+      new_entry = google_service.create_file(params[:tag])
+      `touch entry#{user_id}.txt`
+      `echo "#{params[:body]}" >> entry#{user_id}.txt`
+      reply = google_service.drive.insert_file(new_entry,
+        upload_source: "entry#{user_id}.txt",
+        content_type: 'text/plain')
+      `rm entry#{user_id}.txt`
+      @entry = JournalEntry.create(file_id: reply.id)
+      render json: {created: "success"}
+      # redirect_to journal_entry_path(@entry)
+    else
+      render json: {created: "error"}
+
+    end
   end
 
   def show
@@ -32,6 +37,7 @@ class JournalEntriesController < ApplicationController
   end
 
   def journal_params
-    params.require(:journal_entry).permit(:tag, :body)
+    params.permit!
+    params.except("controller","action","tag","body")
   end
 end
